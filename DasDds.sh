c433d192620e6fe988a00291e65ed21474d24623
Nate <nate.rosenstock@hakai.org>
2020-05-01 16:12:07 -0700

    make docker scripts windows friendly

--- DasDds.sh
+++ DasDds.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-docker run --rm -it \
-  -v "$(pwd)/datasets:/datasets" \
-  -v "$(pwd)/logs:/erddapData/logs" \
-  -v "$(pwd)/config:/usr/local/tomcat/content/erddap" \
-  axiom/docker-erddap:latest \
-  bash -c "cd webapps/erddap/WEB-INF/ && bash DasDds.sh"
-  
+docker exec -it erddap-basic bash -c "cd webapps/erddap/WEB-INF/ && bash DasDds.sh"
--- GenerateDatasetsXml.sh
+++ GenerateDatasetsXml.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-docker run --rm -it \
-  -v "$(pwd)/datasets:/datasets" \
-  -v "$(pwd)/logs:/erddapData/logs" \
-  axiom/docker-erddap:latest \
-  bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
+docker exec -it erddap-basic bash -c "cd webapps/erddap/WEB-INF/ && bash GenerateDatasetsXml.sh -verbose"
